443c98191bfcbe3569dc7cbd59b7118ebc57f2d6
angelzxz1 <angel.zuna@icloud.com>
2022-11-21 02:19:22 -0500

    Created login and register, edited  login, register is just a copy to be modified

---
+++ pages/login.tsx
@@ -0,0 +1,36 @@
+import { Box, Flex, Grid, Input } from "@chakra-ui/react";
+import React from "react";
+
+const LogIn = () => {
+    return (
+        <Flex h="calc(100vh - 3.5rem)" align="center">
+            <Grid
+                templateAreas={`"left right"`}
+                templateColumns="1fr 1fr"
+                templateRows="1fr"
+                h="75%"
+                w="100%"
+                overflow="hidden"
+                borderRadius="2xl"
+            >
+                <Box gridArea="left" bg="pink"></Box>
+                <Grid
+                    gridArea="right"
+                    templateAreas={`"." "mTop" "mBot" "."`}
+                    templateColumns="1fr"
+                    templateRows="repeat(4, 1fr)"
+                >
+                    <Flex gridArea="mTop" direction="column">
+                        <Box ml={4}>Username</Box>
+                        <Input type="text" />
+                    </Flex>
+                    <Flex gridArea="mBot" direction="column">
+                        <Box ml={4}>Password</Box>
+                        <Input type="password" />
+                    </Flex>
+                </Grid>
+            </Grid>
+        </Flex>
+    );
+};
+export default LogIn;
