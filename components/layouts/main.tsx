90273bb45d76c9d667dd81e9152608e6a9b9608b
Angel Zúñiga Navarro <angel.zuna@icloud.com>
2022-11-22 19:05:11 +0000

    Update 6 files

--- components/layouts/main.tsx
+++ components/layouts/main.tsx
@@ -4,34 +4,29 @@ import NavBar from "../navBar";
 import type { Router } from "next/dist/client/router";
 //                  next/app/dist/pages/client/router
 type Props = {
-    children: JSX.Element;
-    router: Router;
-    title: string;
+  children: JSX.Element;
+  router: Router;
+  title: string;
 };
 const Main = ({ children, router, title }: Props) => {
-    return (
-        <Box as="main">
-            <Head>
-                <meta charSet="utf-8" />
-                <meta
-                    name="viewport"
-                    content="initial-scale=1.0, width=device-width"
-                />
-                <link
-                    rel="shortcut icon"
-                    href="/favicon.ico"
-                    type="image/x-icon"
-                />
-                <title>
-                    {title === "notFound" ? " - Page not found" : `- ${title}`}
-                </title>
-            </Head>
-            <NavBar path={router.asPath} />
-            <Container maxW="container.lg" mt={14}>
-                {children}
-            </Container>
-        </Box>
-    );
+  return (
+    <Box as="main">
+      <Head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
+        <title>
+          {title === "notFound"
+            ? "In Touch - Page not found"
+            : `In Touch - ${title}`}
+        </title>
+      </Head>
+      <NavBar path={router.asPath} />
+      <Container maxW="container.lg" mt={14}>
+        {children}
+      </Container>
+    </Box>
+  );
 };
 
 export default Main;
